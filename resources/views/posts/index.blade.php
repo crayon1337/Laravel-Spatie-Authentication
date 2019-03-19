ea74787db56c79cf2c6fdaeb29f453e86f59ae47
Crayon <darkspear123@outlook.com>
2019-03-19 18:14:19 +0200

    Added html parser

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
         <div class="card-body">
             <h5 class="card-title"><a class="card-link" href="{{ route('posts.show', $post->id ) }}"><b>{{ $post->title }}</a></h5>
             <div class="card-subtitle mb-2 text-muted">Page {{ $posts->currentPage() }} of {{ $posts->lastPage() }}</div>
-            <p class="card-text">{{  str_limit($post->body, 100) }} {{-- Limit teaser to 100 characters --}}</p>
+            <p class="card-text">{!!  str_limit($post->body, 100) !!} {{-- Limit teaser to 100 characters --}}</p>
             <a href="{{ route('posts.show', $post->id ) }}" class="btn btn-primary">Read More</a>
         </div>
     @endforeach
